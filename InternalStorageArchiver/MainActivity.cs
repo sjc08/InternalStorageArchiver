847ef53e73b8d0b9584ff0f05d5053d8c85a9cf1
AlexSJC <cn_sjc@outlook.com>
2024-08-22 09:25:16 +0800

    添加项目文件。

---
+++ InternalStorageArchiver/MainActivity.cs
@@ -0,0 +1,13 @@
+namespace InternalStorageArchiver
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
